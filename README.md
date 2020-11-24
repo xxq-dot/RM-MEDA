95197638be581d9d3709071ebf8b801ab18218be
xuxueqing <74091975+xxq-dot@users.noreply.github.com>
2020-11-24 21:57:08 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# RM-MEDA_py
+Python version of RM-MEDA algorithm
+
+Local PCA and Generate: The code is translated from Matlab PlatEMO framework.
+
+
+Requirements:
+
+1- Numpy and matplotlib
+
+2- PyGMO 1.17
---
+++ main.py
@@ -0,0 +1,22 @@
+from RMMEDA import *
+import matplotlib.pylab as plt
+
+
+
+prob = problem.zdt(1) # creating a problem here we used zdt1
+#prob = problem.dtlz(3)
+#prob = problem.kur() 
+#prob = problem.fon() 
+#prob = problem.cec2009(1)
+#prob = problem.cassini_1(objectives=2) 
+pop = population(prob,20)
+
+
+alg = rm_meda(gen = 100, K = 5)
+pop = alg.evolve(pop)
+pop.plot_pareto_fronts() # ploting pareto fronts
+#prob.plot(pop1)
+plt.show() 
+
+
+
